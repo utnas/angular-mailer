05273c545aa16a05ba5790c98cdc3eeef5a6bf26
Dooley <nsewolo@gmail.com>
2014-09-20 12:44:12 -0400

    Mailer view sorting emails down and up

--- app/scripts/mailer.js
+++ app/scripts/mailer.js
@@ -3,9 +3,62 @@
 angular.module('Mailer', [])
     .controller('MainController', function ($scope) {
 
-        $scope.name = "Ari";
-        $scope.sayHello = function () {
-            $scope.greeting = "Hello " + $scope.name;
+        $scope.folders = [
+            { value: "MAILBOX", label: 'Mail box', emails: [
+                { id: 1, from: "Albator", to: "Rudy", subject: "I will be back", date: "20/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis." },
+                { id: 2, from: "Capitaine Flam", to: "Rudy", subject: "Hello from", date: "18/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>." },
+                { id: 3, from: "Pikachu", to: "Rudy", subject: "Pika pika !", date: "15/03/2014", content: "Pika pika ! Chuuuuuu. Pika pika ! Chuuuuuu. Pika pika ! Chuuuuuu. Pika pika ! Pika pika ? Piiiiika Chuuuuuu. Pika pika ! Pikachu. Pika pika pika." },
+                { id: 4, from: "Barbapapa", to: "Rudy", subject: "Hulahup Barbatruc", date: "13/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>." }
+            ]  },
+            { value: "TRASH", label: "Trash", emails: [
+                { id: 5, from: "Candy", to: "Rudy", subject: "Happy birthday", date: "20/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis." },
+                { id: 6, from: "Hiro Nakamura", to: "Rudy", subject: "Konichiwa", date: "18/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>." },
+                { id: 7, from: "Asuka Langley Soryu", to: "Rudy", subject: "Came back", date: "20/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis." }
+            ]  },
+            { value: "SENTS", label: "Sents", emails: [
+                { id: 8, from: "Rudy", to: "Albator", subject: "How are you ?", date: "20/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis." },
+                { id: 9, from: "Rudy", to: "Capitaine Flam", subject: "Gloubiboulga Night", date: "18/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>." }
+            ] },
+            { value: "SPAM", label: "Spam", emails: [
+                { id: 10, from: "Rue du discount", to: "Rudy", subject: "Send me a mail ?", date: "20/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis." },
+                { id: 11, from: "Sofinnoga", to: "Rudy", subject: "I need money ?", date: "18/03/2014", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>." }
+            ] }
+        ];
+
+        $scope.currentFolder = null;
+        $scope.selectedEmail = null;
+
+        $scope.selectFolder = function (folder) {
+            $scope.currentFolder = folder;
+            $scope.selectedEmail = null;
         };
 
+        $scope.selectEmail = function (email) {
+            $scope.selectedEmail = email;
+        };
+
+        $scope.fieldSort = null;
+        $scope.sortDown = false;
+        $scope.sortBySender = function () {
+            if ($scope.fieldSort == 'from') {
+                $scope.sortDown = !$scope.sortDown;
+            } else {
+                $scope.fieldSort = 'from';
+                $scope.sortDown = false;
+            }
+        };
+        $scope.sortByReceiver = function () {
+            if ($scope.fieldSort == 'to') {
+                $scope.sortDown = !$scope.sortDown;
+            } else {
+                $scope.fieldSort = 'to';
+                $scope.sortDown = false;
+            }
+        };
+        $scope.sortBySubject = function () {
+            $scope.fieldSort = 'subject';
+        };
+        $scope.sortByDate = function () {
+            $scope.fieldSort = 'date';
+        };
     });
--- test/spec/controllers/test_mailer.js
+++ test/spec/controllers/test_mailer.js
@@ -1,24 +1,17 @@
 'use strict';
 
 describe('Unit: MainController', function () {
-    // Load the module with MainController
     beforeEach(module('Mailer'));
 
     var ctrl, scope;
-    // inject the $controller and $rootScope services
-    // in the beforeEach block
     beforeEach(inject(function ($controller, $rootScope) {
-        // Create a new scope that's a child of the $rootScope
         scope = $rootScope.$new();
-        // Create the controller
         ctrl = $controller('MainController', {
             $scope: scope
         });
     }));
 
     it('should create $scope.greeting when calling sayHello', function () {
-        expect(scope.greeting).toBeUndefined();
-        scope.sayHello();
-        expect(scope.greeting).toEqual("Hello Ari");
+        expect(scope.selectFolder('MAILBOX').label).toEqual("Hello Ari");
     });
 });
