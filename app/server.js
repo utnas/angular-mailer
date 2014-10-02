180c48ac77c6ff46121386edae7de197661de8f0
Dooley <nsewolo@gmail.com>
2014-10-01 21:14:47 -0400

    preparing the api to get content

--- app/server.js
+++ app/server.js
@@ -1,40 +1,54 @@
 var http = require('http');
-var fs = require('fs');
-var mime = require('mime');
+var express = require('express');
+var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
 var PORT = 3223;
-
-var sendFile = function (res, url) {
-    console.log('Sending file' + url);
-
-    var path = __dirname + '/' + url;
-
-    fs.stat(path, function (err, stats) {
-        if (!err && stats.isFile()) {
-            var flux = fs.createReadStream(path, {
-                flags: 'r',
-                autoClose: true
-            });
-            var mimeType = mime.lookup(path);
-            res.writeHead(200, {'Content-Type': mimeType});
-            flux.pipe(res);
-        } else {
-            send404(res);
-        }
-    });
-};
 
 var send404 = function (res) {
     res.writeHead(404, {'Content-Type': 'text/html'});
     res.end('<h1>Page not found</h1>');
 };
 
-http.createServer(function (req, res) {
-    if (req.url === '/') {
-        res.writeHead(301, {'location': '/mailer.html'});
-        res.end();
-    } else {
-        sendFile(res, req.url);
-    }
-}).listen(PORT);
+var app = express();
 
-console.log('Server started on port' + PORT);
+app.use(serveStatic(__dirname + '/'));
+
+http.createServer(app).listen(PORT);
+
+// API
+
+var api = express();
+
+// Get Folders
+//GET /api/folders
+api.get('/folders', function (req, res) {
+    res.send(
+        {}
+    );
+});
+
+// Get a folder
+//GET /api/folders/idFolder
+api.get('/folders/:idFolder', function (req, res) {
+    res.send(
+        {}
+    );
+});
+
+// Get Email
+// GET /api/folders/folderId/idMail
+api.get('/folders/:idFolder/:idEmail', function (req, res) {
+    res.send(
+        {}
+    );
+});
+
+app.use(bodyParser.json());
+
+// Send email
+// POST /api/send
+api.post('/send', function (req, res) {
+    res.send({succes: true, email: req.body});
+});
+
+app.use("/api/", api);
